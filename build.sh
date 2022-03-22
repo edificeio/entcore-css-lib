259959f507193e52cb9bcef19909346f7e9962b2
Clément Creusat <clement.creusat@gmail.com>
2022-03-22 19:13:10 +0100

    feat: #WB-510

---
+++ build.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+if [ ! -e node_modules ]
+then
+  mkdir node_modules
+fi
+
+if [ -z ${USER_UID:+x} ]
+then
+  export USER_UID=1000
+  export GROUP_GID=1000
+fi
+
+
+clean () {
+  rm -rf node_modules
+}
+
+init () {
+  docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle sh -c "gradle generateTemplate"
+  docker-compose run --rm -u "$USER_UID:$GROUP_GID" node sh -c "npm install"
+}
+
+build () {
+  local extras=$1
+  docker-compose run --rm -u "$USER_UID:$GROUP_GID" node sh -c "npm run release:build"
+  VERSION=`grep "version="  gradle.properties| sed 's/version=//g'`
+  echo "entcore-css-lib=$VERSION `date +'%d/%m/%Y %H:%M:%S'`" >> dist/version.txt
+}
+
+watch () {
+  docker-compose run --rm -u "$USER_UID:$GROUP_GID" node sh -c "npm run dev:watch"
+}
+
+lint () {
+  docker-compose run --rm -u "$USER_UID:$GROUP_GID" node sh -c "npm run dev:lint"
+}
+
+lint-fix () {
+  docker-compose run --rm -u "$USER_UID:$GROUP_GID" node sh -c "npm run dev:lint-fix"
+}
+
+publish () {
+  LOCAL_BRANCH=`echo $GIT_BRANCH | sed -e "s|origin/||g"`
+  docker-compose run --rm -u "$USER_UID:$GROUP_GID" node sh -c "npm publish --tag $LOCAL_BRANCH"
+}
+
+for param in "$@"
+do
+  case $param in
+    clean)
+      clean
+      ;;
+    init)
+      init
+      ;;
+    build)
+      build
+      ;;
+    watch)
+      watch
+      ;;
+    lint)
+      lint
+      ;;
+    lint-fix)
+      lint-fix
+      ;;
+    publish)
+      publish
+      ;;
+    *)
+      echo "Invalid argument : $param"
+  esac
+  if [ ! $? -eq 0 ]; then
+    exit 1
+  fi
+done
